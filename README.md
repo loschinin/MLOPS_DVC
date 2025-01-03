a91de969c990acacb8084b5df4bfc44e988a973c
dmitryloschinin <loschinin@list.ru>
2025-01-03 21:38:34 +0300

    train.csv.dvc, process_data.py, README.md

--- README.md
+++ README.md
@@ -32,20 +32,54 @@
 
 ### Синхронизация с удаленным хранилищем.
 
-- dvc remote add -d myremote s3://mlops2train/data
+      dvc remote add -d myremote s3://mlops2train/data
 
 - Параметры для доступа к Yandex Object Storage:
-- dvc remote modify myremote endpointurl https://storage.yandexcloud.net
-  dvc remote modify myremote access_key_id <your-access-key-id>
-  dvc remote modify myremote secret_access_key <your-secret-access-key>
+  
+      dvc remote modify myremote endpointurl https://storage.yandexcloud.net
+      dvc remote modify myremote access_key_id <your-access-key-id>
+      dvc remote modify myremote secret_access_key <your-secret-access-key>
 
 - Проверка конфигурации 
-- dvc remote list
-  dvc remote modify myremote --list
+
+      dvc remote list
+      dvc remote modify myremote --list
 
 - Загрузка данных в удаленное хранилище
-- dvc push
+
+      dvc push
+
 - Поддягивание данных из удаленного хранилища
-- dvc pull
+
+      dvc pull
+
 
 ### Создание и запуск пайплайна
+
+#### Создадим папку src и в ней файл process_data.py
+Код в этом файле подготавливает данные и настраивает модель BERT для задачи классификации текста: он загружает данные из CSV-файла, проверяет и заполняет пропущенные значения, преобразует тексты и метки в числовой формат, разделяет данные на обучающую и валидационную выборки, токенизирует тексты с использованием токенизатора BERT, создает наборы данных и сохраняет их в файлы. Затем данные загружаются в DataLoader для удобной работы с батчами, определяется устройство для вычислений (CPU или MPS для macOS), и загружается предобученная модель BERT для классификации текста. Код подготавливает всё необходимое для последующего обучения модели.
+
+#### Создание файла dvc.yaml
+
+    stages:
+      process_data:
+        cmd: python src/process_data.py
+        deps:
+          - data/raw/train.csv
+        outs:
+          - data/processed/train_dataset.pt
+          - data/processed/val_dataset.pt
+          
+
+#### Запуск пайплайна
+
+    dvc repro
+
+DVC автоматически проверил изменения в зависимостях и выполнил только те этапы, которые требуют обновления. В директории data появилась поддиректория processed с файлами
+train_dataset.pt и val_dataset.pt:
+
+Это файлы содержат обработанные данные, которые были подготовлены для обучения модели
+
+### Коммит и dvc push
+
+    dvc push
--- src/process_data.py
+++ src/process_data.py
@@ -71,62 +71,7 @@ def main():
     model = BertForSequenceClassification.from_pretrained('bert-base-uncased', num_labels=len(label_encoder.classes_))
     model.to(device)
 
-    # Функция оценки модели
-    def evaluate_model(model, data_loader, device):
-        model.eval()
-        y_pred, y_true = [], []
-        with torch.no_grad():
-            for batch in data_loader:
-                input_ids, attention_mask, labels = batch
-                input_ids = input_ids.to(device)
-                attention_mask = attention_mask.to(device)
-                outputs = model(input_ids, attention_mask=attention_mask)
-                logits = outputs.logits
-                y_pred.extend(torch.argmax(logits, dim=1).cpu().numpy())
-                y_true.extend(labels.cpu().numpy())
-        accuracy = accuracy_score(y_true, y_pred)
-        f1 = f1_score(y_true, y_pred, average='weighted')
-        return accuracy, f1
 
-    # Оптимизатор и планировщик
-    optimizer = torch.optim.AdamW(model.parameters(), lr=5e-5)
-    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=1, gamma=0.1)
-
-    epoch = 1
-    # Обучение модели
-#     model.train()
-#     for epoch in range(epoch):
-#         model.train()
-#         total_loss = 0
-#         for i, batch in enumerate(train_loader):
-#             optimizer.zero_grad()
-#             input_ids, attention_mask, labels = batch
-#             input_ids = input_ids.to(device)
-#             attention_mask = attention_mask.to(device)
-#             labels = labels.to(device)
-#
-#             # Убрали autocast, так как он не поддерживается для MPS
-#             outputs = model(input_ids, attention_mask=attention_mask, labels=labels)
-#             loss = outputs.loss
-#
-#             loss.backward()
-#             optimizer.step()
-#
-#             total_loss += loss.item()
-#             if (i + 1) % 50 == 0:  # Печатаем прогресс каждые 50 батчей
-#                 avg_loss = total_loss / (i + 1)
-#                 # Оценка на валидационной выборке
-#                 val_accuracy, val_f1 = evaluate_model(model, val_loader, device)
-#                 print(f"Epoch {epoch + 1}, Batch {i + 1}/{len(train_loader)}, Loss: {avg_loss:.4f}, Val Accuracy: {val_accuracy:.4f}, Val F1: {val_f1:.4f}")
-#
-#         scheduler.step()  # Обновление learning rate
-#         # Оценка на валидационной выборке после эпохи
-#         val_accuracy, val_f1 = evaluate_model(model, val_loader, device)
-#         print(f"Epoch {epoch + 1} finished. Average Loss: {total_loss / len(train_loader):.4f}, Val Accuracy: {val_accuracy:.4f}, Val F1: {val_f1:.4f}")
-#
-#     # Оценка модели после дообучения
-#     accuracy_after, f1_after = evaluate_model(model, val_loader, device)
-#     print(f"After Fine-tuning - Accuracy: {accuracy_after:.4f}, F1-score: {f1_after:.4f}")
 
 if __name__ == '__main__':
     main()
